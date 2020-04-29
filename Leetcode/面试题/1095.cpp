8c7a188893458f70066cd48c1420978215651396
Super-long <2339824768@qq.com>
2020-04-29 11:53:24 +0800

    daily

---
+++ Leetcode/面试题/1095.cpp
@@ -0,0 +1,43 @@
+// 山脉数组中查找目标值
+#include <bits/stdc++.h>
+using namespace std;
+
+class MountainArray {
+  public:
+    int get(int index);
+    int length();
+};
+
+class Solution {
+    int binary_search(MountainArray &mountain, int target, int l, int r, int key(int)) {
+        target = key(target);
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            int cur = key(mountain.get(mid));
+            if (cur == target)
+                return mid;
+            else if (cur < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int l = 0, r = mountainArr.length() - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1))
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        
+        int peak = l;
+        int index = binary_search(mountainArr, target, 0, peak, [](int x) -> int{return x;}); // 还是比较巧妙的
+        if (index != -1)
+            return index;
+        return binary_search(mountainArr, target, peak + 1, mountainArr.length() - 1, [](int x) -> int{return -x;});
+    }
+};
---
+++ Leetcode/面试题/面试题56-1.cpp
@@ -0,0 +1,11 @@
+// 面试题56 - I. 数组中数字出现的次数
+// 真是妙蛙种子吃着妙脆角妙进了米奇妙妙屋
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> singleNumbers(vector<int>& nums) {
+
+    }
+};
