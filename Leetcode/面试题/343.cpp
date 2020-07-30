8f8293e902bd7c4a32b755d5fc674560007e13b4
Super-long <2339824768@qq.com>
2020-07-30 22:32:33 +0800

    daily

---
+++ Leetcode/面试题/343.cpp
@@ -0,0 +1,18 @@
+// 整数拆分
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector <int> dp(n + 1);
+        for (int i = 2; i <= n; i++) {
+            int curMax = 0;
+            for (int j = 1; j < i; j++) { // 加等于号没有意义 每次结果都是零 但不影响正确性
+                curMax = max(curMax, max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = curMax;
+        }
+        return dp[n];
+    }
+};
