43ead3d90772357ce3d5c5ae213e8d1995132741
Super-long <2339824768@qq.com>
2021-02-28 04:53:00 +0000

    daily

--- Leetcode/春招/二分/154.cpp
+++ Leetcode/春招/二分/154.cpp
@@ -4,6 +4,18 @@ using namespace std;
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-
+        int lhs = 0;
+        int rhs = nums.size() - 1;
+        while(lhs < rhs){
+            int mid = lhs + (rhs - lhs) / 2;
+            if(nums[mid] > nums[rhs]){
+                lhs = mid + 1;
+            } else if(nums[mid] < nums[rhs]){
+                rhs = mid;
+            } else {
+                rhs--;
+            }
+        }
+        return nums[rhs];
     } 
 };
